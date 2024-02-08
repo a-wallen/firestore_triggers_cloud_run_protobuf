14a0734fb03fedd7c9422530a80e260d2e412063
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2024-02-07 23:21:04 -1000

    chore: commit progress

---
+++ install.sh
@@ -0,0 +1,5 @@
+# 1. Activate the dart_frog cli dart pub global activate dart_frog_cli
+dart pub global activate dart_frog_cli
+
+# 2. Now run the dart frog server
+dart_frog dev
--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,47 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+import '../generated/google/events/cloud/datastore/v1/events.pb.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  // Assuming bytes() returns Stream<List<int>>
+  final byteStream = context.request.bytes();
+
+  // Prepare a completer to handle asynchronous operation
+  final completer = Completer<Uint8List>();
+
+  // Create a list to collect all byte chunks
+  List<int> data = [];
+
+  // Listen to the stream
+  byteStream.listen(
+    (List<int> chunk) {
+      // Add each chunk to the collective list
+      data.addAll(chunk);
+    },
+    onDone: () {
+      // Once stream is finished, convert collected list to Uint8List
+      completer.complete(Uint8List.fromList(data));
+    },
+    onError: (Object error) {
+      // Handle errors, if any
+      completer.completeError(error);
+    },
+  );
+
+  try {
+    final requestBody = await completer.future;
+
+    final event = EntityWrittenEvent.fromBuffer(requestBody);
+
+    return Response(body: event.toDebugString());
+  } catch (e) {
+    // Handle the error appropriately
+    return Response.json(
+      body: {'error': e.toString()},
+      statusCode: 500,
+    );
+  }
 }
